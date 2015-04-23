b780ff71a2a4bc7aa6e7c7b6aebd24434b554dcd
Fabio Di Monte <fdimonte@expedia.com>
2015-04-23 11:54:44 +0200

    adde gitignore and minor alignments with newer api

--- js/main.js
+++ js/main.js
@@ -10,16 +10,16 @@ $(document).ready(function(){
 
     isoGrid = isoEngine.addLayer('grid');
 
-    isoEngine.drawGrid(isoGrid);
-    isoEngine.drawAxis(isoGrid,200);
+    isoEngine.draw.grid(isoGrid,null,null,null,true);
+    isoEngine.draw.axis(isoGrid,200,true);
 
     drawSquare(isoGrid,[15,15,0],40,{fillColor:0xff0000,strokeColor:0x00ff00});
 
 });
 
 function drawSquare(stage,origin,size,opt,isIso) {
-    var points = stage.parent.polygonSquare(size);
-    stage.draw.setup(opt);
-    stage.draw.polygon(origin,points,true,isIso);
-    stage.draw.render();
+    var points = stage.parent.get3DPolygonSquare(size);
+    stage._info.draw.setup(opt);
+    stage._info.draw.polygon(points,origin,isIso);
+    stage._info.draw.render();
 }
