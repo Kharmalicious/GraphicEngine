cab9a0167b27548c6f8ea94b18fd21bc2558d0d9
Fabio Di Monte <fdimonte@expedia.com>
2015-04-23 13:09:34 +0200

    code clean

--- js/main.js
+++ js/main.js
@@ -13,12 +13,12 @@ $(document).ready(function(){
     isoEngine.draw.grid(isoGrid,null,null,null,true);
     isoEngine.draw.axis(isoGrid,200,true);
 
-    drawSquare(isoGrid,[15,15,0],40,{fillColor:0xff0000,strokeColor:0x00ff00});
+    drawSquare(isoGrid,[15,15,0],40,{fillColor:0xff0000,strokeColor:0x00ff00},true);
 
 });
 
 function drawSquare(stage,origin,size,opt,isIso) {
-    var points = stage.parent.draw.get3DPolygonSquare(size);
+    var points = stage._engine.draw.get3DPolygonSquare(size);
     stage._info.draw.setup(opt);
     stage._info.draw.polygon(points,origin,isIso);
     stage._info.draw.render();
